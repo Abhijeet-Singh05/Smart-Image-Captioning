8cfe19d2e674179b829fdf39e78fb8489efcddd4
Abhijeet-Singh05 <singh.abhijeet9342@gmail.com>
2025-12-17 15:14:19 +0530

    created post model

---
+++ src/models/post_model.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime, timezone
+
+# Post schema
+class PostModel(BaseModel):
+    image_url: str = Field(..., alias="imageUrl")
+    public_id: str = Field(..., alias="publicId")
+    caption: str
+    hashtags: List[str]
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Config:
+        populate_by_name = True
+        json_schema_extra = {
+            "example":{
+                "imgaeUrl":"https://res.cloudinary...",
+                "caption":"A beautiful sunset",
+                "hashtags": ["#sunset","#nature"]
+            }
+        }
